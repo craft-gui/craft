1dd0a40ebbdd9d1f1e4e3b2abcc03087c6161bca
Austin M. Reppert <austinmreppert@gmail.com>
2024-11-17 19:22:04 -0500

    Clip scroll

--- crates/oku_core/src/elements/container.rs
+++ crates/oku_core/src/elements/container.rs
@@ -77,7 +77,8 @@ impl Element for Container {
         let scroll_height = computed_content_height - border_top;
 
         let max_scroll_y = scroll_height - client_height;
-        
+
+        // Background
         renderer.draw_rect(
             Rectangle::new(
                 computed_x_transformed,
@@ -132,11 +133,17 @@ impl Element for Container {
             border_color,
         );
 
-
+        renderer.push_layer(Rectangle::new(
+            computed_x_transformed + border_left,
+            computed_y_transformed + border_top,
+            computed_width - (border_right + border_left),
+            computed_height - (border_top + border_bottom),
+        ));
         for (index, child) in self.common_element_data.children.iter_mut().enumerate() {
             let child2 = taffy_tree.child_at_index(root_node, index).unwrap();
             child.internal.draw(renderer, font_system, taffy_tree, child2, element_state);
         }
+        renderer.pop_layer();
 
         // scrollbar
         let scroll_track_color = Color::rgba(100, 100, 100, 255);
