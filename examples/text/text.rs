3c46b4855c392cad8c3dc8f45e2ac81642435f93
Austin M. Reppert <austinmreppert@gmail.com>
2024-12-31 15:08:03 -0500

    Use material symbols

--- examples/text/text.rs
+++ examples/text/text.rs
@@ -17,7 +17,7 @@ use oku_core::RendererType::Software;
 pub struct TextState {}
 
 const FONT: &str =
-    "https://github.com/google/material-design-icons/raw/refs/heads/master/font/MaterialIcons-Regular.ttf";
+    "https://github.com/google/material-design-icons/raw/refs/heads/master/variablefont/MaterialSymbolsOutlined%5BFILL%2CGRAD%2Copsz%2Cwght%5D.ttf";
 
 impl Component for TextState {
     type Props = ();
@@ -27,7 +27,7 @@ impl Component for TextState {
             .flex_direction(FlexDirection::Column)
             .push(Text::new("Hello, World!").id("hello_text"))
             .push(Font::new(ResourceIdentifier::Url(FONT.to_string())))
-            .push(Text::new("search home").font_family("Material Icons").font_size(24.0))
+            .push(Text::new("search home").font_family("Material Symbols Outlined").font_size(24.0))
             .push(TextInput::new("Test").flex_direction(FlexDirection::Column).id("text_input"))
             .component()
     }
