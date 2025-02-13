32371f2137c343b182cd140304dd84a3fe3d6493
Austin M. Reppert <austinmreppert@gmail.com>
2025-02-13 17:49:07 -0500

    Downgrade winit

--- wasm-build.sh
+++ wasm-build.sh
@@ -1,9 +1,12 @@
+set -e
+
+
 #rustup target add wasm32-unknown-unknown
 #cargo install -f wasm-bindgen-cli
 #cargo install simple-http-server
 
-cargo build --target wasm32-unknown-unknown --example counter
+cargo build --target wasm32-unknown-unknown --example request
 
-wasm-bindgen target/wasm32-unknown-unknown/debug/examples/counter.wasm --target web --no-typescript --out-dir target/generated --out-name counter
+wasm-bindgen target/wasm32-unknown-unknown/debug/examples/request.wasm --target web --no-typescript --out-dir target/generated --out-name request
 
 simple-http-server . -c wasm,html,js -i --coep --coop --ip 127.0.0.1
