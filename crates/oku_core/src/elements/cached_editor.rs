a73b3753ba67c5625f7072fc2583852ed8d4f802
NoahR02 <noahreppert95@gmail.com>
2025-03-22 00:01:47 -0400

    Add some inline documentation for the cached editor

--- crates/oku_core/src/elements/cached_editor.rs
+++ crates/oku_core/src/elements/cached_editor.rs
@@ -1,27 +1,86 @@
 use crate::elements::layout_context::{MetricsRaw, TextHashKey};
-use crate::elements::text::{hash_text, AttributesRaw, TextHashValue};
 use crate::style::Style;
-use cosmic_text::{Action, Buffer, Edit, Editor, FontSystem, Motion, Shaping};
+use cosmic_text::{Action, Attrs, Buffer, Edit, Editor, Family, FontSystem, Motion, Shaping, Weight};
 use std::collections::HashMap;
+use std::hash::Hasher;
+use rustc_hash::FxHasher;
+
+#[derive(Clone)]
+pub struct TextHashValue {
+    pub computed_width: f32,
+    pub computed_height: f32,
+    pub buffer: Buffer,
+}
+
+pub struct AttributesRaw {
+    pub(crate) font_family_length: u8,
+    pub(crate) font_family: Option<[u8; 64]>,
+    weight: Weight,
+}
+
+impl AttributesRaw {
+    pub(crate) fn from(style: &Style) -> Self {
+        let font_family = if style.font_family_length() == 0 {
+            None
+        } else {
+            Some(style.font_family_raw())
+        };
+        Self {
+            font_family_length: style.font_family_length(),
+            font_family,
+            weight: Weight(style.font_weight().0),
+        }
+    }
+
+    pub(crate) fn to_attrs(&self) -> Attrs {
+        let mut attrs = Attrs::new();
+        if let Some(font_family) = &self.font_family {
+            attrs.family = Family::Name(
+                std::str::from_utf8(&font_family[..self.font_family_length as usize]).unwrap()
+            );
+            attrs.weight = self.weight;
+        }
+        attrs
+    }
+
+}
+
+impl PartialEq for AttributesRaw {
+    fn eq(&self, other: &Self) -> bool {
+        self.font_family == other.font_family &&
+            self.font_family_length == other.font_family_length &&
+            self.weight == other.weight
+    }
+}
+
+pub(crate) fn hash_text(text: &String) -> u64 {
+    let mut text_hasher = FxHasher::default();
+    text_hasher.write(text.as_ref());
+    text_hasher.finish()
+}
 
 pub struct CachedEditor<'a> {
     pub text_hash: u64,
     pub cached_text_layout: HashMap<TextHashKey, TextHashValue>,
+    /// The key to get the last computed `Buffer` and its width and height.
     pub last_key: Option<TextHashKey>,
+    /// The internal cosmic-text editor that we wrap, so that we can do caching.
     pub editor: Editor<'a>,
-    // Attributes
+    /// Stores Attrs fields as integers for hashing. 
     pub(crate) attributes: AttributesRaw,
-    // Metrics
+    /// Stores Metric fields as integers for hashing.
     pub(crate) metrics: MetricsRaw,
 }
 
 impl CachedEditor<'_> {
+    /// Get the last cache entry using the `last_key`.
     pub(crate) fn get_last_cache_entry(&self) -> &TextHashValue {
         let key = self.last_key.unwrap();
         &self.cached_text_layout[&key]
     }
 
-    #[allow(clippy::too_many_arguments)]
+    /// Measure the width and height of the text and cache the result.
+    /// This method may be called up to 3-5 times by Taffy for a single Text element.
     pub(crate) fn measure(
         &mut self,
         known_dimensions: taffy::Size<Option<f32>>,
@@ -31,6 +90,8 @@ impl CachedEditor<'_> {
         let cache_key = TextHashKey::new(known_dimensions, available_space);
         self.last_key = Some(cache_key);
 
+        // Currently we are caching the `Buffer` which is memory hungry, so we should clear the cache if this grows to be too big.
+        // Measure is called 3-5 times.
         if self.cached_text_layout.len() > 3 {
             self.cached_text_layout.clear();
         }
@@ -48,7 +109,7 @@ impl CachedEditor<'_> {
             });
             self.editor.shape_as_needed(font_system, true);
 
-            // Determine measured size of text
+            // Measure the size of the text.
             let cached_text_layout_value = self.editor.with_buffer(|buffer| {
                 let (width, total_lines) = buffer
                     .layout_runs()
@@ -67,6 +128,7 @@ impl CachedEditor<'_> {
                 height: cached_text_layout_value.computed_height,
             };
 
+            // Update the cache.
             self.cached_text_layout.insert(cache_key, cached_text_layout_value);
             size
         }
@@ -103,16 +165,18 @@ impl CachedEditor<'_> {
             || reload_fonts
             || attributes != self.attributes;
         let size_changed = metrics != self.metrics;
-
+        
         if text_changed || size_changed {
             self.clear_cache();
         }
 
+        // In `measure()` we will call `set_metrics_and_size()`, so save the metrics for later.
         if size_changed {
             self.metrics = metrics;
         }
 
         if text_changed {
+            // Rebuild the cosmic-text lines. 
             self.editor.with_buffer_mut(|buffer| {
                 buffer.set_text(font_system, text, attributes.to_attrs(), Shaping::Advanced);
             });
@@ -122,6 +186,7 @@ impl CachedEditor<'_> {
         }
     }
     
+    /// Clears the cache.
     pub(crate) fn clear_cache(&mut self) {
         self.cached_text_layout.clear();
         self.last_key = None;
