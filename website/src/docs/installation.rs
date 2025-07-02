9c29624eb13c313c0ecf1cd6f067ad45f5784ba2
NoahR02 <noahreppert95@gmail.com>
2025-07-02 00:18:47 -0400

    Simplify the docs code

--- website/src/docs/installation.rs
+++ website/src/docs/installation.rs
@@ -1,12 +1,10 @@
+use crate::docs::docs_template;
+use crate::docs::markdown_viewer::{MarkdownViewer, MarkdownViewerProps};
 use crate::WebsiteGlobalState;
-use craft::components::{Component, ComponentSpecification, Context};
-use craft::elements::{Container, ElementStyles, Text};
-use craft::events::{CraftMessage, Message};
-use craft::style::{Display, FlexDirection};
+use craft::components::{Component, ComponentSpecification, Context, Props};
 
 #[derive(Default)]
 pub(crate) struct InstallationPage {
-    markdown: Option<ComponentSpecification>
 }
 
 impl Component for InstallationPage {
@@ -15,30 +13,10 @@ impl Component for InstallationPage {
     type Message = ();
 
     fn view(context: &mut Context<Self>) -> ComponentSpecification {
-        if let Some(markdown) = &context.state().markdown {
-            Container::new()
-                .display(Display::Flex)
-                .flex_direction(FlexDirection::Column)
-                .push(markdown.clone())
-                .component()
-        } else {
-            Container::new()
-                .display(Display::Flex)
-                .flex_direction(FlexDirection::Column)
-                .push(Text::new("Loading installation instructions..."))
-                .component()
-        }
-    }
-
-    fn update(context: &mut Context<Self>) {
-        if let Message::CraftMessage(CraftMessage::LinkClicked(link)) = context.message() {
-            craft::components::open(link);
-            context.event_mut().prevent_propagate();
-        }
-
-        if let Message::CraftMessage(CraftMessage::Initialized) = context.message() {
-            let installation = craft::markdown::render_markdown(include_str!("markdown/installation.md"));
-            context.state_mut().markdown = Some(installation);
-        }
+        docs_template()
+            .push(MarkdownViewer::component().props(Props::new(MarkdownViewerProps {
+                markdown_text: include_str!("markdown/installation.md").to_string()
+            })))
+            .component()
     }
 }
