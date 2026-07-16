96bd23c5f1decd535633eeebb1521450d99643bc
NoahR02 <noahreppert95@gmail.com>
2026-07-16 00:43:44 -0400

    Use getters for getting the width/height of an image resource

--- crates/craft_resource_manager/src/image.rs
+++ crates/craft_resource_manager/src/image.rs
@@ -4,3 +4,13 @@ use image::RgbaImage;
 pub struct ImageResource {
     pub image: RgbaImage,
 }
+
+impl ImageResource {
+    pub fn get_width(&self) -> u32 {
+        self.image.width()
+    }
+
+    pub fn get_height(&self) -> u32 {
+        self.image.height()
+    }
+}
